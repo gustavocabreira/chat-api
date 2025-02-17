2e6044f6cbc5d3d509ccb855bea525e5d24c34ad
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-17 14:03:29 -0300

    test: all feature tests have the suffix "Test"

---
+++ tests/Feature/TestsHaveSuffixTestTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\File;
+
+test('all feature tests have the suffix "Test"', function () {
+    $testsPath = base_path('tests/Feature');
+    $files = File::allFiles($testsPath);
+
+    $testsWithoutSuffix = [];
+
+    foreach ($files as $file) {
+        $className = $file->getFilenameWithoutExtension();
+        if (! str_ends_with($className, 'Test')) {
+            $testsWithoutSuffix[] = $className;
+        }
+    }
+
+    expect($testsWithoutSuffix)
+        ->toBeEmpty("The following tests do not have the suffix 'Test': ".implode(', ', $testsWithoutSuffix));
+});
