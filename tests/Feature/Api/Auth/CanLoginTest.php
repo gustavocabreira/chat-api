8dfa143e1b1e26e0f37e4dfd8b5a5a105d70c9c7
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-17 13:55:49 -0300

    test: it should return unprocessable entity when trying to login with an invalid payload

--- tests/Feature/Api/Auth/CanLoginTest.php
+++ tests/Feature/Api/Auth/CanLoginTest.php
@@ -17,3 +17,28 @@ test('it should be able to login', function () {
 
     $this->assertDatabaseHas('users', $user->toArray());
 });
+
+dataset('invalid_payload', [
+    'empty email' => [
+        ['email' => ''],
+        ['email' => 'The email field is required.'],
+    ],
+    'empty password' => [
+        ['password' => ''],
+        ['password' => 'The password field is required.'],
+    ],
+    'invalid email' => [
+        ['email' => 'not an email', 'password' => 'L@rav3l1!'],
+        ['email' => 'The email field must be a valid email address.'],
+    ],
+]);
+
+test('it should return unprocessable entity when trying to login with an invalid payload', function ($payload, $expectedErrors) {
+    $response = $this->postJson(route('api.auth.login'), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors($key = array_keys($expectedErrors));
+
+    $response->assertJsonPath("errors.$key[0].0", $expectedErrors[$key[0]]);
+})->with('invalid_payload');
